231a181d5067c76eaede3de51b5f4d24d9df61ae
Jinhan <wlsgks333@naver.com>
2025-06-11 14:42:43 +0900

    fix : code review 반영 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "../globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>PandaMarket</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
