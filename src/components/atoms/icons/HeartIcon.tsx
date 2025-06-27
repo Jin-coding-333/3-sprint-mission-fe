0c527156caa7602b4521de4f1566253369dd7d15
Jinhan <wlsgks333@naver.com>
2025-06-28 01:23:37 +0900

    refactor: HeartIcon 컴포넌트의 구조 개선 - div 요소 제거 및 Image 컴포넌트에 스타일 적용

--- src/components/atoms/icons/HeartIcon.tsx
+++ src/components/atoms/icons/HeartIcon.tsx
@@ -8,14 +8,12 @@ export default function HeartIcon({ type = "pink" }: HeartIconProps) {
   const src = `/icons/ic_heart_${type}.svg`;
 
   return (
-    <div className="w-[22px] h-[22px] md:w-[30px] md:h-[30px]">
-      <Image
-        className="object-contain"
-        src={src}
-        alt="heart icon"
-        width={30}
-        height={30}
-      />
-    </div>
+    <Image
+      className="w-[22px] h-[22px] md:w-[30px] md:h-[30px]"
+      src={src}
+      alt="heart icon"
+      width={30}
+      height={30}
+    />
   );
 }
