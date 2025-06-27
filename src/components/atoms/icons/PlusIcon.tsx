cb6655cc7b5f55c52c5ec87b61f41aba2dd0e4f6
Jinhan <wlsgks333@naver.com>
2025-06-28 02:03:37 +0900

    refactor: PlusIcon 컴포넌트의 코드 간소화 - 이미지 소스 변수를 직접 사용하여 가독성 향상

--- src/components/atoms/icons/PlusIcon.tsx
+++ src/components/atoms/icons/PlusIcon.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function PlusIcon() {
-  const src = `/icons/ic_plus.svg`;
-
-  return <Image src={src} alt="plus icon" width={46} height={46} />;
+  return (
+    <Image src="/icons/ic_plus.svg" alt="plus icon" width={46} height={46} />
+  );
 }
