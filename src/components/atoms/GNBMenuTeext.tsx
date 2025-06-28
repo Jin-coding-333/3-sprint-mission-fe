2a75f690170ad70a26343ed592b246329d121669
Jinhan <wlsgks333@naver.com>
2025-06-29 00:06:41 +0900

    feat: GNBMenuText 컴포넌트 추가 - 텍스트를 표시하는 새로운 메뉴 텍스트 컴포넌트를 구현하여 UI 구성 요소 확장

---
+++ src/components/atoms/GNBMenuTeext.tsx
@@ -0,0 +1,11 @@
+interface GNBMenuTextProps {
+  text: string;
+}
+
+export default function GNBMenuText({ text }: GNBMenuTextProps) {
+  return (
+    <div className="flex items-center cursor-pointer w-[70px] h-[70px] md:w-[108px] md:h-[68px] text-lg md:text-2lg text-secondary-600 hover:text-primary-100 font-bold transition-colors duration-200">
+      <span>{text}</span>
+    </div>
+  );
+}
