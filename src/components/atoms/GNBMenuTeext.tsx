fd0c5c8b79f8cd3243e1712138cf37dfb257b7e2
Jinhan <wlsgks333@naver.com>
2025-07-01 14:52:24 +0900

    fix: GNBMenuText 컴포넌트의 너비 수정 - 메뉴 텍스트의 너비를 70px에서 56px로 변경하여 디자인 일관성 향상

--- src/components/atoms/GNBMenuTeext.tsx
+++ src/components/atoms/GNBMenuTeext.tsx
@@ -4,7 +4,7 @@ interface GNBMenuTextProps {
 
 export default function GNBMenuText({ text }: GNBMenuTextProps) {
   return (
-    <div className="flex items-center cursor-pointer w-[70px] h-[70px] md:w-[108px] md:h-[68px] text-lg md:text-2lg text-secondary-600 hover:text-primary-100 font-bold transition-colors duration-200">
+    <div className="flex items-center cursor-pointer w-[56px] h-[70px] md:w-[108px] md:h-[68px] text-lg md:text-2lg text-secondary-600 hover:text-primary-100 font-bold transition-colors duration-200">
       <span>{text}</span>
     </div>
   );
