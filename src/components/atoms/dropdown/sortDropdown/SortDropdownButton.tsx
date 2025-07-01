fbea9be3ee57eaa758c741f7c85cddf18c784abc
Jinhan <wlsgks333@naver.com>
2025-07-02 02:08:19 +0900

    feat: SortDropdownButton 컴포넌트 추가 - 정렬 드롭다운 버튼 UI 요소 구현

---
+++ src/components/atoms/dropdown/sortDropdown/SortDropdownButton.tsx
@@ -0,0 +1,28 @@
+import Icon from "../../icons/Icon";
+
+interface SortDropdownButtonProps {
+  sortDropdownText: string;
+}
+
+export default function SortDropdownButton({
+  sortDropdownText = "최신순",
+}: SortDropdownButtonProps) {
+  return (
+    <div className={sortDropdownButtonBox}>
+      <Icon
+        type="sort"
+        className="block relative md:hidden w-[24px] h-[24px]"
+      />
+      <div className={sortDropdownButtonTextWithIcon}>
+        <span>{sortDropdownText}</span>
+        <Icon type="arrow_down" className="hidden md:block" />
+      </div>
+    </div>
+  );
+}
+
+const sortDropdownButtonBox =
+  "w-[42px] h-[42px] md:w-[130px] md:h-[42px] p-[9px] md:px-[20px] py-[12px] rounded-[12px] border-secondary-200 border-[1px] cursor-pointer";
+
+const sortDropdownButtonTextWithIcon =
+  "hidden md:w-full md:h-full md:flex md:justify-between items-center text-secondary-800 text-lg";
