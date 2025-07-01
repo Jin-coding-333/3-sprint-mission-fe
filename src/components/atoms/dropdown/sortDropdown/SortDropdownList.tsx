5364354bd4aa06697f11bd1c2c26715f2836753b
Jinhan <wlsgks333@naver.com>
2025-07-02 02:19:14 +0900

    style: SortDropdownList 컴포넌트의 스타일 수정 - 최신순 항목에 경계선 색상 변경 및 드롭다운 박스의 경계선 색상 조정

--- src/components/atoms/dropdown/sortDropdown/SortDropdownList.tsx
+++ src/components/atoms/dropdown/sortDropdown/SortDropdownList.tsx
@@ -3,14 +3,18 @@ import cn from "@/utils/cn";
 export default function SortDropdownList() {
   return (
     <div className={sortDropdownListBox}>
-      <div className={cn(sortDropdownListText, "border-b")}>최신순</div>
+      <div
+        className={cn(sortDropdownListText, "border-b border-secondary-200")}
+      >
+        최신순
+      </div>
       <div className={sortDropdownListText}>좋아요순</div>
     </div>
   );
 }
 
 const sortDropdownListBox =
-  "absolute top-20px right-0 mt-[10px] w-[130px] h-[84px] rounded-[12px] border-secondary-300 border-[1px] cursor-pointer";
+  "absolute top-20px right-0 mt-[10px] w-[130px] h-[84px] rounded-[12px] border-secondary-200 border-[1px] cursor-pointer";
 
 const sortDropdownListText =
   "flex items-center justify-center w-full h-[42px] text-lg text-secondary-800";
