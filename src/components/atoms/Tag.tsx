0b3ec29b2bb6e47b3516dc1e9a0e0bfab0c79148
Jinhan <wlsgks333@naver.com>
2025-07-01 15:29:09 +0900

    feat: Tag 컴포넌트 추가 - 텍스트와 아이콘을 포함한 태그 UI 요소를 구현하여 사용자 인터페이스 확장

---
+++ src/components/atoms/Tag.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface TagProps {
+  type: "editable" | "readonly";
+  text: string;
+}
+
+export default function Tag({ type, text }: TagProps) {
+  return (
+    <div
+      className={` ${
+        type === "editable" ? "pl-[16px] pr-[12px]" : "px-[16px]"
+      } flex items-center justify-center w-fit h-[36px] py-[5px] rounded-[26px] cursor-pointer bg-secondary-100`}
+    >
+      <span
+        className={`${
+          type === "editable" ? "mr-[8px]" : ""
+        } text-secondary-800 text-lg`}
+      >
+        #{text}
+      </span>
+      <Image
+        className={`${type === "readonly" ? "hidden" : "block"}`}
+        src="/icons/ic_X.svg"
+        alt="tag close button"
+        width={20}
+        height={20}
+      />
+    </div>
+  );
+}
