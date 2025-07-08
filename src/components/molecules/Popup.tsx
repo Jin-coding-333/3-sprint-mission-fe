470b0d399f6c8413f06e61fc38f1a8115e5a48b4
Jinhan <wlsgks333@naver.com>
2025-07-08 15:22:36 +0900

    feat: Popup 컴포넌트 추가 - 자식 요소를 포함하고 확인 버튼을 갖춘 팝업 UI 요소 구현

---
+++ src/components/molecules/Popup.tsx
@@ -0,0 +1,39 @@
+import Backdrop from "@/components/atoms/Backdrop";
+import cn from "@/utils/cn";
+import Button from "@/components/atoms/button/Button";
+
+interface PopupProps {
+  children: React.ReactNode;
+}
+
+export default function Popup({ children }: PopupProps) {
+  const PopupStyle = {
+    popupBoxSize: "w-[327px] h-[220px] md:w-[540px] md:h-[250px]",
+    textStyle: " text-lg md:text-2lg",
+    buttonStyle: "w-[120px] h-[50px] md:w-[165px] md:h-[50px] mt-[40px]",
+  };
+
+  return (
+    <div>
+      <Backdrop>
+        <div
+          className={cn(
+            "flex flex-col items-center justify-center bg-white rounded-[12px]",
+            PopupStyle.popupBoxSize
+          )}
+        >
+          <h1 className={cn("text-secondary-800", PopupStyle.textStyle)}>
+            {children}
+          </h1>
+          <Button
+            variant="primary"
+            shape="rectangle"
+            className={PopupStyle.buttonStyle}
+          >
+            확인
+          </Button>
+        </div>
+      </Backdrop>
+    </div>
+  );
+}
