d7ded54b8323fc0d29a92e337ce88c1e6632a9bc
Jinhan <wlsgks333@naver.com>
2025-07-11 16:25:12 +0900

    fix: BadgeIcon 컴포넌트의 alt 속성 수정 - 'bedge icon'을 'badge icon'으로 변경

--- src/components/atoms/icons/BadgeIcon.tsx
+++ src/components/atoms/icons/BadgeIcon.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function BadgeIcon() {
   return (
-    <Image src="/icons/ic_badge.svg" alt="bedge icon" width={102} height={30} />
+    <Image src="/icons/ic_badge.svg" alt="badge icon" width={102} height={30} />
   );
 }
