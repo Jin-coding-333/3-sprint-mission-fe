817493959b5f5fa356c984baa2dcec077834152f
Jinhan <wlsgks333@naver.com>
2025-07-12 16:46:21 +0900

    feat: Textarea 컴포넌트에 id 속성 추가 및 aria-label 속성 추가 - 접근성 향상을 위한 개선

--- src/components/atoms/field/Textarea.tsx
+++ src/components/atoms/field/Textarea.tsx
@@ -3,23 +3,33 @@ import { fieldStyles } from "./field.style";
 import { TextareaProps } from "./field.type";
 
 export default function Textarea({
+  id,
   name,
   placeholder,
   className,
   onChange,
   value,
-  rows,
+  rows = 5,
   required,
 }: TextareaProps) {
   return (
-    <textarea
-      name={name}
-      value={value}
-      rows={rows}
-      required={required}
-      placeholder={placeholder}
-      className={cn(fieldStyles.base, fieldStyles.textarea, className)}
-      onChange={onChange}
-    ></textarea>
+    <div className="flex flex-col gap-1">
+      <textarea
+        id={id}
+        name={name}
+        value={value}
+        rows={rows}
+        required={required}
+        placeholder={placeholder}
+        className={cn(
+          fieldStyles.base,
+          fieldStyles.textarea,
+          "resize-y",
+          className
+        )}
+        onChange={onChange}
+        aria-label={placeholder}
+      ></textarea>
+    </div>
   );
 }
