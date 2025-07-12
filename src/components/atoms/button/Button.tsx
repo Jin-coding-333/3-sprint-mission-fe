a0e620eecaa9ae38bd8ced53f3f6d1058c804352
Jinhan <wlsgks333@naver.com>
2025-07-12 16:45:51 +0900

    feat: Button 컴포넌트의 variant 속성을 styleVariant로 변경 및 스타일 관리 방식 개선 - 버튼 스타일을 객체로 정리하여 가독성 향상

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -2,7 +2,7 @@ import cn from "@/utils/cn";
 
 interface ButtonProps {
   shape?: "rectangle" | "round";
-  variant: "primary" | "secondary" | "danger" | "outline";
+  styleVariant: "primary" | "secondary" | "danger" | "outline";
   onClick?: () => void;
   disabled?: boolean;
   className?: string;
@@ -11,36 +11,20 @@ interface ButtonProps {
 
 export default function Button({
   shape = "rectangle",
-  variant,
+  styleVariant,
   onClick,
   disabled,
   className,
   children,
 }: ButtonProps) {
-  const baseStyle =
-    "flex items-center justify-center w-[357px] h-[56px] text-lg";
-  const variantStyle = {
-    primary: disabled
-      ? "bg-secondary-400 text-secondary-100"
-      : "bg-primary-100 text-secondary-100 hover:bg-primary-200",
-    secondary:
-      "bg-secondary-50 text-primary-100 border border-solid border-primary-100",
-    danger: "bg-error text-secondary-100",
-    outline: "bg-secondary-50 text-error border border-solid border-error",
-  };
-  const shapeStyle = {
-    rectangle: "rounded-[12px]",
-    round: "rounded-[26px]",
-  };
-
   return (
     <button
       className={cn(
-        baseStyle,
-        variantStyle[variant],
-        shapeStyle[shape],
+        buttonStyle.base,
+        buttonStyle[styleVariant],
+        buttonStyle.shape[shape],
         className,
-        disabled && "opacity-50 cursor-not-allowed"
+        disabled && "opacity-50"
       )}
       onClick={onClick}
       disabled={disabled}
@@ -49,3 +33,16 @@ export default function Button({
     </button>
   );
 }
+
+const buttonStyle = {
+  base: "flex items-center justify-center w-[357px] h-[56px] text-lg",
+  primary: "bg-primary-100 text-secondary-100",
+  secondary:
+    "bg-secondary-50 text-primary-100 border border-solid border-primary-100",
+  danger: "bg-error text-secondary-100",
+  outline: "bg-secondary-50 text-error border border-solid border-error",
+  shape: {
+    rectangle: "rounded-[12px]",
+    round: "rounded-[26px]",
+  },
+};
