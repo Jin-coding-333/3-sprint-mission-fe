80499f47548685a86273d971e7a7b0fdf58ea4de
Jinhan <wlsgks333@naver.com>
2025-07-14 11:33:23 +0900

    feat: Modal 컴포넌트 리팩토링 및 접근성 개선 - 버튼 클릭 시 팝업 닫기 기능 추가 및 스타일 수정

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,72 +1,63 @@
+"use client";
+
+import Backdrop from "@/components/atoms/Backdrop";
 import cn from "@/utils/cn";
 import Button from "@/components/atoms/button/Button";
-import Icon from "@/components/atoms/icons/Icon";
+import { useRef, useState } from "react";
+import { useEscapeClose } from "@/hooks/useEscapeClose";
 
 interface ModalProps {
-  variant: "primary" | "danger";
-  noClick?: () => void;
-  yesClick?: () => void;
+  onClick?: () => void;
   children: React.ReactNode;
 }
+/**
+ * @param onClick 팝업 확인 버튼 클릭 시 실행 함수
+ * @param children 팝업 알림 Text
+ */
+export default function Modal({ onClick, children }: ModalProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEscapeClose(() => setIsOpen(false));
 
-export default function Modal({
-  variant,
-  noClick,
-  yesClick,
-  children,
-}: ModalProps) {
-  const PrimaryButtons = [
-    { label: "아니요", variant: "secondary", onClick: noClick },
-    { label: "확인", variant: "primary", onClick: yesClick },
-  ];
-
-  const DangerButtons = [
-    { label: "취소", variant: "outline", onClick: noClick },
-    { label: "네", variant: "danger", onClick: yesClick },
-  ];
-
-  const ModalButtons = variant === "primary" ? PrimaryButtons : DangerButtons;
+  const handleClick = () => setIsOpen(false);
 
   return (
-    <div className={ModalStyle.ModalPosition}>
-      <div
-        className={cn(
-          "flex flex-col items-center justify-center pt-[5px] bg-white rounded-[12px] shadow-lg ",
-          ModalStyle.ModalBoxSize
-        )}
-      >
-        <Icon type={`check-${variant === "primary" ? "blue" : "red"}`} />
-        <h1 className={cn("mt-[20px]", ModalStyle.textStyle)}>{children}</h1>
-        <div className="flex items-center justify-center gap-4">
-          {ModalButtons.map((button) => {
-            return (
-              <Button
-                key={button.label}
-                variant={
-                  button.variant as
-                    | "primary"
-                    | "danger"
-                    | "secondary"
-                    | "outline"
-                }
-                shape="rectangle"
-                className={ModalStyle.buttonStyle}
-                onClick={button.onClick}
-              >
-                {button.label}
-              </Button>
-            );
-          })}
+    <div>
+      <Backdrop>
+        <div
+          className={cn(
+            "flex flex-col items-center justify-center bg-white rounded-[12px]",
+            ModalStyle.modalBoxSize
+          )}
+          ref={modalRef}
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-title"
+        >
+          <h1
+            className={cn("text-secondary-800", ModalStyle.textStyle)}
+            role="heading"
+            aria-level={2}
+            id="modal-title"
+          >
+            {children}
+          </h1>
+          <Button
+            styleVariant="primary"
+            shape="rectangle"
+            className={ModalStyle.buttonStyle}
+            onClick={handleClick}
+          >
+            확인
+          </Button>
         </div>
-      </div>
+      </Backdrop>
     </div>
   );
 }
 
 const ModalStyle = {
-  ModalPosition:
-    "fixed top-[20%] left-0 right-0 z-20 flex flex-col items-center justify-center",
-  ModalBoxSize: "w-[300px] h-[205px]",
-  textStyle: "text-secondary-800 text-lg md:text-2lg",
-  buttonStyle: "w-[90px] h-[50px] mt-[20px]",
+  modalBoxSize: "w-[327px] h-[220px] md:w-[540px] md:h-[250px]",
+  textStyle: " text-lg md:text-2lg",
+  buttonStyle: "w-[120px] h-[50px] md:w-[165px] md:h-[50px] mt-[40px]",
 };
