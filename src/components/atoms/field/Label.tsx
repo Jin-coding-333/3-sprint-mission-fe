19bdcd73d77e3c43984188b7b81070e154603dec
Jinhan <wlsgks333@naver.com>
2025-07-14 16:09:07 +0900

    feat: Label 컴포넌트의 className 속성 수정 - 불필요한 margin 제거로 스타일 간소화

--- src/components/atoms/field/Label.tsx
+++ src/components/atoms/field/Label.tsx
@@ -9,10 +9,7 @@ export default function Label({ label, htmlFor, className }: LabelProps) {
   return (
     <label
       htmlFor={htmlFor}
-      className={cn(
-        "text-secondary-800 text-lg md:text-2lg mb-[8px] md:mb-[16px]",
-        className
-      )}
+      className={cn("text-secondary-800 text-lg md:text-2lg", className)}
     >
       {label}
     </label>
