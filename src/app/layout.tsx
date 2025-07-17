5d48c6549fef9ddbe7b07e29b79cc3c3d863e977
Jinhan <wlsgks333@naver.com>
2025-07-18 00:14:45 +0900

    feat: RootLayout에 GNB 컴포넌트 추가 - 상단 내비게이션 바를 포함하여 레이아웃 구성 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import Provider from "@/providers/_provider";
+import GNB from "@/components/organisms/gnb";
 
 export default function RootLayout({
   children,
@@ -14,8 +15,11 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>PandaMarket</title>
       </head>
-      <body>
-        <Provider>{children}</Provider>
+      <body className="pt-[80px]">
+        <Provider>
+          <GNB />
+          {children}
+        </Provider>
       </body>
     </html>
   );
