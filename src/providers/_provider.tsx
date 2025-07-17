bf2e23d7156e5042b09452c4afbe012439eb2b5e
Jinhan <wlsgks333@naver.com>
2025-07-18 00:15:05 +0900

    feat: QueryClient 초기화 방식 변경 - useState를 사용하여 상태로 관리하도록 수정

--- src/providers/_provider.tsx
+++ src/providers/_provider.tsx
@@ -1,20 +1,24 @@
-'use client';
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 3,
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 10,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 3,
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 10,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
-export default function Provider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
