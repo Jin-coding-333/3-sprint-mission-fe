2c65ace9e93ab83b793df9633506342f91e9663a
Jinhan <wlsgks333@naver.com>
2025-07-18 14:19:36 +0900

    refactor: PlusIcon 컴포넌트에 props 추가 - width와 height를 props로 받아 동적으로 아이콘 크기 조정 가능

--- src/components/atoms/icons/PlusIcon.tsx
+++ src/components/atoms/icons/PlusIcon.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import React from "react";
 
-export default function PlusIcon() {
+interface PlusIconProps {
+  width?: number;
+  height?: number;
+}
+
+export default function PlusIcon({ width = 46, height = 46 }: PlusIconProps) {
   return (
-    <Image src="/icons/ic_plus.svg" alt="plus icon" width={46} height={46} />
+    <Image
+      src="/icons/ic_plus.svg"
+      alt="plus icon"
+      width={width}
+      height={height}
+    />
   );
 }
