594bdbe32de8956f7190b66806a6c342bc708ee0
Jinhan <wlsgks333@naver.com>
2025-07-18 14:18:37 +0900

    refactor: ArrowIcon 컴포넌트의 alt 속성 및 lazy loading 추가 - 화살표 방향에 따라 alt 텍스트 수정 및 이미지 로딩 최적화

--- src/components/atoms/icons/ArrowIcon.tsx
+++ src/components/atoms/icons/ArrowIcon.tsx
@@ -7,6 +7,15 @@ interface ArrowIconProps {
 
 export default function ArrowIcon({ type, color = "black" }: ArrowIconProps) {
   const src = `/icons/ic_arrow_${type}_${color}.svg`;
+  const direction = type === "left" ? "왼쪽" : "오른쪽";
 
-  return <Image src={src} alt="arrow icon" width={7} height={7} />;
+  return (
+    <Image
+      src={src}
+      alt={`${direction} 화살표`}
+      width={7}
+      height={7}
+      loading="lazy"
+    />
+  );
 }
