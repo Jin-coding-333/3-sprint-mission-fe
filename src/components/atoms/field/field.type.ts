8a24282629ad38d0932f7d8eff067be1d7e5e95a
Jinhan <wlsgks333@naver.com>
2025-07-18 14:18:17 +0900

    refactor: InputProps 및 TextareaProps 인터페이스 수정 - BaseFieldProps에 React의 HTML 속성 추가 및 불필요한 속성 제거

--- src/components/atoms/field/field.type.ts
+++ src/components/atoms/field/field.type.ts
@@ -1,18 +1,12 @@
 interface BaseFieldProps {
-  id: string;
-  name?: string;
-  value?: string;
-  required?: boolean;
-  disabled?: boolean;
-  placeholder?: string;
-  className?: string;
+  error?: boolean;
+  helperText?: string;
 }
 
-export interface InputProps extends BaseFieldProps {
-  type?: React.HTMLInputTypeAttribute;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-}
+export interface InputProps
+  extends BaseFieldProps,
+    React.InputHTMLAttributes<HTMLInputElement> {}
 
-export type TextareaProps = BaseFieldProps &
-  React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export interface TextareaProps
+  extends BaseFieldProps,
+    React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
