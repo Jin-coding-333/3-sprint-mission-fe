dddcbf58f7fbded813781e9dd510e9f733179855
Jinhan <wlsgks333@naver.com>
2025-07-20 00:05:05 +0900

    feat: BottomSection 컴포넌트 추가 - 하단 섹션에 제목과 이미지 컴포넌트 포함

---
+++ src/pages/landing/components/BottomSection.tsx
@@ -0,0 +1,8 @@
+export default function BottomSection() {
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <h1 className="text-4xl font-bold">BottomSection</h1>
+      {/* 이미지 컴포넌트 */}
+    </div>
+  );
+}
