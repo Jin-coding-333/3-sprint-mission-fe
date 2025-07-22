f86bd11d7fea35e36341a15262cc1359ca6679e3
Jinhan <wlsgks333@naver.com>
2025-07-22 16:02:30 +0900

    feat: HeroSection 컴포넌트 스타일 개선 - 레이아웃 변경 및 텍스트 크기 조정으로 시각적 요소 향상

--- src/pages/landing/components/HeroSection.tsx
+++ src/pages/landing/components/HeroSection.tsx
@@ -2,14 +2,15 @@ import Button from "@/components/atoms/button/Button";
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col items-center justify-between">
-      <div>
-        <h1 className="text-4xl font-bold">Hero Section</h1>
-        <Button shape="rectangle" styleVariant="primary">
-          구경하러 가기
-        </Button>
+    <section className="w-full min-h-[500px] bg-[#cfe5ff] flex items-center justify-center">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col items-center gap-6 text-black">
+          <h1 className="text-5xl font-bold">Hero Section</h1>
+          <Button shape="rectangle" styleVariant="primary">
+            구경하러 가기
+          </Button>
+        </div>
       </div>
-      {/* 이미지 컴포넌트 */}
-    </div>
+    </section>
   );
 }
