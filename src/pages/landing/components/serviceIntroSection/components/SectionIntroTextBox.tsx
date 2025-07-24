f2e2ef096377c258cc13436e590d8869b3991bf9
Jinhan <wlsgks333@naver.com>
2025-07-24 15:40:06 +0900

    refactor: SectionIntroTextBox 컴포넌트 스타일 수정 - 텍스트 박스 크기를 조정하여 레이아웃 개선

--- src/pages/landing/components/serviceIntroSection/components/SectionIntroTextBox.tsx
+++ src/pages/landing/components/serviceIntroSection/components/SectionIntroTextBox.tsx
@@ -4,7 +4,6 @@ interface SectionIntroTextBoxProps {
   badge: string;
   title: string;
   description: string;
-  textColor: string;
   className?: string;
 }
 
@@ -15,7 +14,7 @@ export default function SectionIntroTextBox({
   className,
 }: SectionIntroTextBoxProps) {
   return (
-    <div className={cn("w-full h-fit", className)}>
+    <div className={cn("w-[275px] h-[275px]", className)}>
       <div className={cn("flex flex-col gap-2")}>
         <span className="text-lg md:text-2lg text-primary-100">{badge}</span>
         <span className="mb-2 md:text:3xl text-2xl font-bold text-secondary-700">
