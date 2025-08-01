b6d76a71615110218a1636594379c7f035d50bad
Jinhan <wlsgks333@naver.com>
2025-08-01 18:31:00 +0900

    style: SectionIntroImage 컴포넌트의 이미지 클래스 수정 - 최소 너비 설정으로 반응형 디자인 개선 및 불필요한 주석 제거

--- src/pages/landing/components/serviceIntroSection/components/SectionIntroImage.tsx
+++ src/pages/landing/components/serviceIntroSection/components/SectionIntroImage.tsx
@@ -8,19 +8,14 @@ export default function SectionIntroImage({
   validSection,
 }: SectionIntroImageProps) {
   return (
-    // <div
-    // // className=
-    // // "relative w-[345px] h-[260px] md:w-[670px] md:h-[524px] lg:w-[590px] lg:h-[445px]"
-    // >
     <Image
       width={670}
       height={524}
-      className="w-[145px] h-[160px] md:w-[670px] md:h-[524px] lg:w-[590px] lg:h-[445px]"
+      className="min-w-[344px] h-[259px] md:w-[670px] md:h-[524px] lg:w-[590px] lg:h-[445px]"
       src={SectionIntroImageData[validSection].src}
       alt={SectionIntroImageData[validSection].alt}
       priority
     />
-    // </div>
   );
 }
 
@@ -38,7 +33,3 @@ const SectionIntroImageData = {
     alt: "랜딩 페이지 세 번째 이미지",
   },
 };
-
-// const SectionIntroImageSize = {
-//   width:
-// }
