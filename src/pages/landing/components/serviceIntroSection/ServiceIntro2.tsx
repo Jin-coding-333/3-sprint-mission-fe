9e6f80540a627197aa44cc3bea38c5d4ee00d745
Jinhan <wlsgks333@naver.com>
2025-08-01 18:31:36 +0900

    style: ServiceIntro2 컴포넌트 개선 - SectionIntroContainerBox로 JSX 구조 변경 및 텍스트 내용 수정

--- src/pages/landing/components/serviceIntroSection/ServiceIntro2.tsx
+++ src/pages/landing/components/serviceIntroSection/ServiceIntro2.tsx
@@ -1,18 +1,17 @@
-import SectionIntroImage from "./components/SectionIntroImage";
-import SectionIntroTextBox from "./components/SectionIntroTextBox";
+import SectionIntroContainerBox from './components/SectionIntroContainerBox';
+import SectionIntroImage from './components/SectionIntroImage';
+import SectionIntroTextBox from './components/SectionIntroTextBox';
 
 export default function ServiceIntro2() {
   return (
-    <div className="flex justify-center items-center h-[470px]">
-      <div className="flex justify-between items-start">
-        <SectionIntroImage validSection="section2" />
-        <SectionIntroTextBox
-          badge="Hot item"
-          title="인기 상품을 확인해 보세요"
-          description="가장 HOT한 중고거래 물품을
-판다 마켓에서 확인해 보세요"
-        />
-      </div>
-    </div>
+    <SectionIntroContainerBox validSection="section2">
+      <SectionIntroImage validSection="section2" />
+      <SectionIntroTextBox
+        validSection="section2"
+        badge="Search"
+        title="구매를 원하는 상품을 검색하세요"
+        description={'구매하고 싶은 물품을 검색해서 \n쉽게 찾아보세요'}
+      />
+    </SectionIntroContainerBox>
   );
 }
