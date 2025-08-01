15cb74e4dbac07b1553fa9f63ad56753b59496a4
Jinhan <wlsgks333@naver.com>
2025-08-01 18:31:08 +0900

    style: SectionIntroTextBox 컴포넌트 개선 - 유효한 섹션 추가 및 스타일 일관성 향상, JSX 구조 수정

--- src/pages/landing/components/serviceIntroSection/components/SectionIntroTextBox.tsx
+++ src/pages/landing/components/serviceIntroSection/components/SectionIntroTextBox.tsx
@@ -1,29 +1,46 @@
-import cn from "@/utils/cn";
+import cn from '@/utils/cn';
 
 interface SectionIntroTextBoxProps {
+  validSection?: 'section1' | 'section2' | 'section3';
   badge: string;
-  title: string;
+  title: string | React.ReactNode;
   description: string;
   className?: string;
 }
 
 export default function SectionIntroTextBox({
+  validSection,
   badge,
   title,
   description,
   className,
 }: SectionIntroTextBoxProps) {
   return (
-    <div className={cn("w-[275px] h-[275px]", className)}>
-      <div className={cn("flex flex-col gap-2")}>
-        <span className="text-lg md:text-2lg text-primary-100">{badge}</span>
-        <span className="mb-2 md:text:3xl text-2xl font-bold text-secondary-700">
-          {title}
-        </span>
-        <span className="text-secondary-700 text-lg md:text-2lg">
-          {description}
-        </span>
-      </div>
+    <div
+      className={cn(
+        SectionIntroTextBoxStyle.container,
+        validSection === 'section2' ? SectionIntroTextBoxStyle.section2 : '',
+        className
+      )}
+    >
+      {/* Badge */}
+      <span className={cn(SectionIntroTextBoxStyle.badge)}>{badge}</span>
+
+      {/* Big Text */}
+      <h2 className={cn(SectionIntroTextBoxStyle.title)}>{title}</h2>
+
+      {/* Small Text */}
+      <span className={cn(SectionIntroTextBoxStyle.description)}>{description}</span>
     </div>
   );
 }
+
+const SectionIntroTextBoxStyle = {
+  container: 'w-[330px] h-[275px] flex flex-col gap-2',
+  badge: 'text-lg md:text-2lg text-primary-100 font-bold',
+  title:
+    'mb-2 md:text:3xl lg:text-[40px] text-2xl lg:leading-normal font-bold text-secondary-700 w-[330px] lg:break-words lg:whitespace-pre-line',
+  description:
+    'text-secondary-700 text-lg md:text-2lg lg:text-2xl w-[290px] break-words whitespace-pre-line',
+  section2: 'items-end text-right',
+};
