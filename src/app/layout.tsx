8842c2c6571170d47c563c443be5a8f9a6f0f59f
Jinhan <wlsgks333@naver.com>
2025-08-13 16:54:28 +0900

    style: favicon.svg 추가 - 사이트의 파비콘을 추가하여 브라우저 탭에서의 식별성 향상

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import "./globals.css";
-import Provider from "@/providers/_provider";
-import GNB from "@/components/organisms/gnb";
-import Footer from "@/components/organisms/footer";
+import React from 'react';
+import './globals.css';
+import Provider from '@/providers/_provider';
+import GNB from '@/components/organisms/gnb';
+import Footer from '@/components/organisms/footer';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>PandaMarket</title>
+        <link rel="icon" href="/favicon.svg" />
       </head>
       <body className="w-full h-full pt-[80px]">
         <Provider>
