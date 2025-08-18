098d4a82fb4561291446b532a8ffa95343f26e74
Jinhan <wlsgks333@naver.com>
2025-08-18 17:38:54 +0900

    feat: 로그인 및 회원가입 페이지 컴포넌트 추가 - AuthLayout을 사용하여 각각의 페이지 레이아웃 구성

---
+++ src/app/(route)/auth/log-in/page.tsx
@@ -0,0 +1,9 @@
+import AuthLayout from "@/pages/auth/components/AuthLayout";
+
+export default function Login() {
+  return (
+    <AuthLayout type="login">
+      <form></form>
+    </AuthLayout>
+  );
+}
---
+++ src/app/(route)/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import AuthLayout from "@/pages/auth/components/AuthLayout";
+
+export default function Signup() {
+  return (
+    <AuthLayout type="signup">
+      <form></form>
+    </AuthLayout>
+  );
+}
