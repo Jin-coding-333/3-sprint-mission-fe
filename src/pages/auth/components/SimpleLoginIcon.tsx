e36db73ccc275d490e4f42f8038f8261faf56ae1
Jinhan <wlsgks333@naver.com>
2025-08-18 18:03:51 +0900

    style: SimpleLoginIcon 컴포넌트 코드 스타일 통일 - import 문 및 JSX 요소의 문자열 스타일을 일관되게 수정

--- src/pages/auth/components/SimpleLoginIcon.tsx
+++ src/pages/auth/components/SimpleLoginIcon.tsx
@@ -1,11 +1,16 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 interface SimpleLoginIconProps {
-  type: 'google' | 'kakao';
+  type: "google" | "kakao";
 }
 
 export default function SimpleLoginIcon({ type }: SimpleLoginIconProps) {
   return (
-    <Image src={`/icons/ic_login_${type}.svg`} alt={`${type} icon image`} width={42} height={42} />
+    <Image
+      src={`/icons/ic_login_${type}.svg`}
+      alt={`${type} icon image`}
+      width={42}
+      height={42}
+    />
   );
 }
