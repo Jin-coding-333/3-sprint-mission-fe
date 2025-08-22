faee04a6b91549c7402258b53de90914a32bd2f3
Jinhan <wlsgks333@naver.com>
2025-08-22 15:30:57 +0900

    fix: fieldStyles의 error 스타일 수정 - border 클래스를 추가하여 오류 표시 개선

--- src/components/atoms/field/field.style.ts
+++ src/components/atoms/field/field.style.ts
@@ -2,5 +2,5 @@ export const fieldStyles = {
   base: "w-full bg-secondary-50 rounded-md border-none text-secondary-800 text-lg placeholder:text-secondary-400 placeholder:text-lg px-[24px] py-[16px]",
   input: "h-[56px]",
   textarea: "h-[104px]",
-  error: "border-error",
+  error: "border border-error",
 };
