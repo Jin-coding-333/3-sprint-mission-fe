e2a0aaba4186441a574a04335030bbe503044ab1
Jinhan <wlsgks333@naver.com>
2025-08-25 15:20:22 +0900

    fix: EyeIcon에서 이미지 경로를 상수로 변경하여 가독성 향상

--- src/components/atoms/icons/EyeIcon.tsx
+++ src/components/atoms/icons/EyeIcon.tsx
@@ -1,5 +1,7 @@
 import cn from "@/utils/cn";
 import Image from "next/image";
+import visible from "/public/icons/ic_eye-visible.svg";
+import invisible from "/public/icons/ic_eye-invisible.svg";
 
 interface EyeIconProps {
   type: "visible" | "invisible";
@@ -8,10 +10,18 @@ interface EyeIconProps {
 }
 
 export default function EyeIcon({ type, className, onClick }: EyeIconProps) {
-  const src = `/icons/ic_eye-${type}.svg`;
+  const src = type === "visible" ? visible : invisible;
 
   return (
-    <button className={cn("w-fit h-fit", className)} onClick={onClick}>
+    <button
+      type="button"
+      className={cn(
+        "w-fit h-fit",
+        className,
+        src === visible ? "bottom-[20px]" : "bottom-[16px]"
+      )}
+      onClick={onClick}
+    >
       <Image src={src} alt="eye icon" width={24} height={24} />
     </button>
   );
