d350ed083fa2c3aba566d6a8065de598c4861ab0
Jinhan <wlsgks333@naver.com>
2025-08-26 16:55:22 +0900

    fix: SignUpFormGroup에서 회원가입 성공 및 실패 시 모달 메시지 추가

--- src/pages/auth/components/SignUpFormGroup.tsx
+++ src/pages/auth/components/SignUpFormGroup.tsx
@@ -2,12 +2,31 @@
 
 import Button from "@/components/atoms/button/Button";
 import InputGroup from "@/components/molecules/inputGroup";
-import { useForm, FieldErrors, FieldError } from "react-hook-form";
+import { useForm, FieldErrors } from "react-hook-form";
 import { useSignUp } from "@/hooks/auth/useAuth";
 import { SignUpCredentials } from "@/services/auth/authApi";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Modal from "@/components/molecules/Modal";
 
 export default function SignUpFormGroup() {
-  const { mutate: signUp } = useSignUp();
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const router = useRouter();
+  const { mutate: signUp } = useSignUp({
+    onSuccess: () => {
+      setShowModal(true);
+      setModalMessage("가입이 완료되었습니다.");
+      setIsSuccess(true);
+    },
+    onError: (error) => {
+      setShowModal(true);
+      setModalMessage(error.message || "회원가입에 실패했습니다.");
+      setIsSuccess(false);
+    },
+  });
   const {
     register,
     handleSubmit,
@@ -29,70 +48,93 @@ export default function SignUpFormGroup() {
   };
 
   return (
-    <form
-      className="w-full flex flex-col gap-6"
-      onSubmit={handleSubmit(onSubmit, onError)}
-    >
-      <InputGroup
-        type="email"
-        register={register}
-        validation={{
-          required: "이메일을 입력해주세요",
-          pattern: {
-            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-            message: "이메일 형식이 올바르지 않습니다",
-          },
-        }}
-        errors={errors.email}
-      />
-      <InputGroup
-        type="nickname"
-        register={register}
-        validation={{
-          required: "닉네임을 입력해주세요",
-          minLength: {
-            value: 2,
-            message: "닉네임은 최소 2자 이상이어야 합니다",
-          },
-          maxLength: {
-            value: 10,
-            message: "닉네임은 최대 10자까지 가능합니다",
-          },
-        }}
-        errors={errors.nickname}
-      />
-      <InputGroup
-        type="password"
-        register={register}
-        validation={{
-          required: "비밀번호를 입력해주세요",
-          pattern: {
-            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-            message:
-              "비밀번호는 최소 8자리 이상이며 영문자와 숫자가 포함되어야 합니다",
-          },
-        }}
-        errors={errors.password}
-      />
-      <InputGroup
-        type="passwordConfirmation"
-        register={register}
-        validation={{
-          required: "비밀번호 확인을 입력해주세요",
-          validate: (value: string) =>
-            value === password || "비밀번호가 일치하지 않습니다",
-        }}
-        errors={errors.passwordConfirmation}
-      />
-      <Button
-        type="submit"
-        styleVariant="primary"
-        shape="round"
-        className="w-full"
-        disabled={!isValid}
+    <>
+      <form
+        className="w-full flex flex-col gap-6"
+        onSubmit={handleSubmit(onSubmit, onError)}
       >
-        회원가입
-      </Button>
-    </form>
+        <InputGroup
+          type="email"
+          register={register}
+          validation={{
+            required: "이메일을 입력해주세요",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "이메일 형식이 올바르지 않습니다",
+            },
+          }}
+          errors={errors.email}
+        />
+        <InputGroup
+          type="nickname"
+          register={register}
+          validation={{
+            required: "닉네임을 입력해주세요",
+            minLength: {
+              value: 2,
+              message: "닉네임은 최소 2자 이상이어야 합니다",
+            },
+            maxLength: {
+              value: 10,
+              message: "닉네임은 최대 10자까지 가능합니다",
+            },
+          }}
+          errors={errors.nickname}
+        />
+        <InputGroup
+          type="password"
+          register={register}
+          validation={{
+            required: "비밀번호를 입력해주세요",
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+              message:
+                "비밀번호는 최소 8자리 이상이며 영문자와 숫자가 포함되어야 합니다",
+            },
+          }}
+          errors={errors.password}
+        />
+        <InputGroup
+          type="passwordConfirmation"
+          register={register}
+          validation={{
+            required: "비밀번호를 다시 한 번 입력해주세요",
+            validate: (value: string) =>
+              value === password || "비밀번호가 일치하지 않습니다",
+          }}
+          errors={errors.passwordConfirmation}
+        />
+        <Button
+          type="submit"
+          styleVariant="primary"
+          shape="round"
+          className="w-full"
+          disabled={!isValid}
+        >
+          회원가입
+        </Button>
+        <Button
+          type="button"
+          styleVariant="outline"
+          shape="round"
+          className="w-full"
+          onClick={() => setShowModal(true)}
+        >
+          로그인
+        </Button>
+      </form>
+      {showModal && (
+        <Modal
+          onClick={() => {
+            setShowModal(false);
+            if (isSuccess) {
+              router.push("/auth/log-in");
+            }
+          }}
+        >
+          {modalMessage}
+        </Modal>
+      )}
+    </>
   );
 }
