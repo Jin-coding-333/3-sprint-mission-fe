d80dd2da90d3dbfc9d5455302e8fc2f7f64908ef
Jinhan <wlsgks333@naver.com>
2025-08-26 16:50:20 +0900

    fix: useInputGroup 함수의 타입 매개변수를 추가하여 제네릭 지원

--- src/hooks/components/useInputGroup.ts
+++ src/hooks/components/useInputGroup.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { INPUT_GROUP_TYPE } from "../../components/molecules/inputGroup/constants";
 
-export function useInputGroup(type: string) {
+export function useInputGroup<T extends string>(type: T) {
   const [inputValue, setInputValue] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
